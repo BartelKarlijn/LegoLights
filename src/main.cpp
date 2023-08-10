ebad1e5cde6cec5db5c0d02d4db3148fbd1054b2
BartelKarlijn <legoreus@gmail.com>
2023-08-10 18:39:43 +0200

    test er uit gooien

--- src/main.cpp
+++ src/main.cpp
@@ -15,27 +15,6 @@ void loop() {
   // put your main code here, to run repeatedly:
 
   // strip aan/uit
-  Println("vast rondje");
-  ledstrip[0] = CRGB::Red;
-  ledstrip[1] = CRGB::Black;
-  FastLED.show();
-  delay(500);
-
-  ledstrip[0] = CRGB::Black;
-  ledstrip[1] = CRGB::Red;
-  FastLED.show();
-  delay(500);
-
-  ledstrip[0] = CRGB::DeepPink;
-  ledstrip[1] = CRGB::Black;
-  FastLED.show();
-  delay(500);
-
-  ledstrip[0] = CRGB::Black;
-  ledstrip[1] = CRGB::DeepPink;
-  FastLED.show();
-  delay(500);
-
   Println("OLI_PLAF");
   aan(STR_OLI_0PLAF);
   uit(STR_OLI_0TRAP);
