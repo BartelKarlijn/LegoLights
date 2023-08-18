2e7c4eb12f0838ffa3844165a41adf483ba52537
BartelKarlijn <legoreus@gmail.com>
2023-08-18 17:14:55 +0200

    webserver kant klaargezet

---
+++ src/wifi/on_file.h
@@ -0,0 +1,31 @@
+void on_file(AsyncWebServerRequest *request) {
+  Println("File list requested");
+  String logmessage;
+  if (request->hasParam("name") && request->hasParam("action")) {
+    const char *fileName = request->getParam("name")->value().c_str();
+    const char *fileAction = request->getParam("action")->value().c_str();
+
+    logmessage = "Requested file " + String(fileName) + "&action=" + String(fileAction);
+
+    if (!SPIFFS.exists(fileName)) {
+      Println(logmessage + " ERROR: file does not exist");
+      request->send(400, "text/plain", "ERROR: file does not exist");
+    } else {
+      Println(logmessage + " file exists");
+      if (strcmp(fileAction, "download") == 0) {
+        logmessage += " downloaded";
+        request->send(SPIFFS, fileName, "application/octet-stream");
+      } else if (strcmp(fileAction, "delete") == 0) {
+        logmessage += " deleted";
+        SPIFFS.remove(fileName);
+        request->send(200, "text/plain", "Deleted File: " + String(fileName));
+      } else {
+        logmessage += " ERROR: invalid action param supplied";
+        request->send(400, "text/plain", "ERROR: invalid action param supplied");
+      }
+      Println(logmessage);
+  }
+  } else {
+    request->send(400, "text/plain", "ERROR: name and action params required");
+  }
+}
--- src/wifi/setup_AsyncWebserver.h
+++ src/wifi/setup_AsyncWebserver.h
@@ -5,86 +5,55 @@ void setup_AsyncWebserver(){
   // Analoog gaan we de WebSerial opstarten, zodat je kan zien wat er gebeurt
   WebSerial.begin(&webserver);
 
+  // if url isn't found
+  webserver.onNotFound([](AsyncWebServerRequest *request) {
+    Println("Wifi page not found");
+    request->send(404, "text/plain", "Not found");
+  });
+
   // Route for root / web page (controller)
   webserver.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
-    request->send(SPIFFS, "/index.html", String(), false, html_processorRoot);
-  });
-
-  // Route for Wifi Passwoord onderhoud
-  webserver.on("/wificfg", HTTP_GET, [](AsyncWebServerRequest *request) {
-    Print("Wifi config pagina");
-    request->send(SPIFFS, "/index.html", String(), false, html_processorWifi);
-  });
-
-  // Opvangen als wifi data bewaard worden
-  webserver.on("/wifisave", HTTP_GET, [](AsyncWebServerRequest *request) {
-    Println("Wifi connection parameters");
-    on_wifisave(request);
-    request->send(SPIFFS, "/index.html", String(), false, html_processorWifi);
-  });
-
-  // Route for strip configuratie
-  webserver.on("/maintain_strip", HTTP_GET, [](AsyncWebServerRequest *request) {
-    Println("strip configuratie opgeroepen");
-    request->send(SPIFFS, "/maintain_strip.html", "text/html");
-  });
-
-  // Route voor getstrip.
-  webserver.on("/getStrip", HTTP_GET, [](AsyncWebServerRequest *request) {
-    //Println("getstrip binnen gekregen"); geen print want we krijgen er zo 10 binnen
-    request->send(200, "text/plain", on_getstrip(request));
-  });
-
-  webserver.on("/style.css", HTTP_GET, [](AsyncWebServerRequest *request) {
-    request->send(SPIFFS, "/style.css", "text/plain");
-  });
-    webserver.on("/jquery-3.7.0.min.js", HTTP_GET, [](AsyncWebServerRequest *request) {
-    request->send(SPIFFS, "/jquery-3.7.0.min.js", "text/javascript");
+    Print("Root requested");
+    //request->send(SPIFFS, "/index.html", String(), false, html_processorRoot);
+    request->send(SPIFFS, "/index.html", "text/html");
   });
   webserver.on("/favicon.ico", HTTP_GET, [](AsyncWebServerRequest *request) {
     request->send(SPIFFS, "/favicon.ico", "image/png");
   });
-
-
- // Verwerk als er op een knop wordt gedrukt
-  webserver.on("/knop", HTTP_GET, [](AsyncWebServerRequest *request) {
-    String IDknopString;
-    uint16_t IDknop;
-    // GET input1 value on <ESP_IP>/update?output=<inputMessage1>&state=<inputMessage2>
-    if (request->hasParam(PARAM_output)) {
-      IDknopString = request->getParam(PARAM_output)->value();
-      IDknop = IDknopString.toInt();
-    }
-    else {
-      IDknop = 0;
-    }
-
-    Print("knop= ");
-    Println(IDknop);
-    switch (IDknop) {
-    case id_Modedo:  //mode
-//      operationMode = buttonChangeUp(operationMode, -1, "mode", OPERATION_DESC_COUNT);
-      break;
-    case id_Modeup:
-//      operationMode = buttonChangeUp(operationMode, +1, "mode", OPERATION_DESC_COUNT);
-      break;
-    //Save Config
-    case id_SaveConfig:
-      buttonChangeConfig();
-//      distributeCommand(COMMAND_SETSAV, 0, 0, 0);
-      break;
-    //Restart
-    case id_Restart:
-      buttonChangeRestart();
-      break;
-
-    default:
-      Println("Impossible IDknop");
-      break;
-    }  // einde switch
-    
-    request->send(200, "text/plain", "OK");
-
+  webserver.on("/file", HTTP_GET, [](AsyncWebServerRequest * request){
+    Print("file requested");
+    on_file(request);
+  });
+  webserver.on("/fileList", HTTP_GET, [](AsyncWebServerRequest * request){
+    Print("fileList requested");
+    request->send(200, "text/plain", listFiles(true));
+  });
+  webserver.on("/fileManagement", HTTP_GET, [](AsyncWebServerRequest *request) {
+    Print("FileManagement requested");
+    request->send(SPIFFS, "/fileManagement.html", "text/html");
+  });
+  webserver.on("/jquery-3.7.0.min.js", HTTP_GET, [](AsyncWebServerRequest *request) {
+    request->send(SPIFFS, "/jquery-3.7.0.min.js", "text/javascript");
+  });
+  webserver.on("/maintain_strip", HTTP_GET, [](AsyncWebServerRequest *request) {
+    Println("strip configuratie opgeroepen");
+    request->send(SPIFFS, "/maintain_strip.html", "text/html");
+  });
+  webserver.on("/getStrip", HTTP_GET, [](AsyncWebServerRequest *request) {
+    //Println("getstrip binnen gekregen"); geen print want we krijgen er zo 10 binnen
+    request->send(200, "text/plain", on_getstrip(request));
+  });
+  webserver.on("/style.css", HTTP_GET, [](AsyncWebServerRequest *request) {
+    request->send(SPIFFS, "/style.css", "text/plain");
+  });
+  webserver.on("/wificfg", HTTP_GET, [](AsyncWebServerRequest *request) {
+    Print("Wifi config pagina");
+    request->send(SPIFFS, "/index.html", String(), false, html_processorWifi);
+  });
+  webserver.on("/wifisave", HTTP_GET, [](AsyncWebServerRequest *request) {
+    Println("Wifi connection parameters");
+    on_wifisave(request);
+    request->send(SPIFFS, "/index.html", String(), false, html_processorWifi);
   });
 
   // Start server
