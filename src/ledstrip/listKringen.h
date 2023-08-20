7af57a9cf74cc7fd117b864b2f2f0f5cde739445
BartelKarlijn <legoreus@gmail.com>
2023-08-20 20:05:31 +0200

    kringen proper oplijsten

--- src/ledstrip/listKringen.h
+++ src/ledstrip/listKringen.h
@@ -4,21 +4,14 @@ String listKringen(bool ishtml) {
   String returnText = "";
   Println("Listing kringen van ledstrip");
 
-  if (ishtml) {
-    returnText += "<table><tr><th align='left'>ID</th><th align='left'>Beschrijving</th></tr>";
-  }
   for (size_t i = 0; i < STRIP_NR_ITEMS; i++) {
     // telkens ID en beschrijving
     if (ishtml) {
-      returnText += "<tr align='left'><td>" + String(i) + "</td>";
-      returnText += "<td> href= '/page_maintain_strip?kring=" + String(i) + "'" + String(kring[i].desc) + "</td>";
+      returnText += "<a href= '/page_maintain_strip?kring=" + String(i) + "' class='button button-center'>" + String(kring[i].desc) + "</a>";
     }
     else {
       returnText += "ID: "+  String(i) + " Beschrijving: " + String(kring[i].desc);
     }
-  }
-  if (ishtml) {
-    returnText += "</table>";
   }
   return returnText;
 }
