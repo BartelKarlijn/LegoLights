1d993a564ea076250e356c4cf63a9eb895876486
BartelKarlijn <legoreus@gmail.com>
2023-08-24 20:25:38 +0200

    pfs corrigeren

--- src/main/show_fps.h
+++ src/main/show_fps.h
@@ -1,12 +1,11 @@
 #pragma once
 void show_fps() {
+  double fps;
   if(currentMillis > fpsMillis) {
     fpsMillis = currentMillis + SHOWFPSEVERY;
-//    fps = cntfps / SHOWFPSEVERY * 1000.00 ;
-    Print(cntfps);
-    Print(" fps/");
-    Print(SHOWFPSEVERY / 1000);
-    Println("sec");
+    fps = double(cntfps) / double(SHOWFPSEVERY) * 1000.00 ;
+    Print(String(fps));
+    Println(" fps");
 
     cntfps = 1;  // we willen geen 0 om eventuele delingen door 0 te vermijden
   }
