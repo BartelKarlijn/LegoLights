ce02c32ec516151400f03a468f45a826eba0635c
BartelKarlijn <legoreus@gmail.com>
2023-09-03 14:54:31 +0200

    loadanimatie

--- src/ledstrip/loadAnimatie.h
+++ src/ledstrip/loadAnimatie.h
@@ -3,7 +3,8 @@ void loadAnimatie() {
   String msgReceive;
   // Eerst ini file lezen, en terugvallen op default als niet gelukt.
   for (size_t i = 0; i < STRIP_NR_ITEMS; i++) {
-    if (fileLoad2Kring(i) = "File does not exist") {
+    if (fileLoad2Kring(i) == "File does not exist") {
+      Println("default voor " + String(i)); delay(100);
       msgReceive = fileDefault2Kring(i);
     };
   }
