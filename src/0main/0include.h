f9f3178963bcff425b260e90771dc92e60012e5a
BartelKarlijn <legoreus@gmail.com>
2025-03-08 14:44:13 +0100

    save eprom extern gezet

--- src/0main/0include.h
+++ src/0main/0include.h
@@ -3,6 +3,7 @@
 
 // External libraries
 // read and save data
+#include <Preferences.h>     // om data in eprom op te slaan
 #include <SPIFFS.h>        // om het filesysteem van ESP32 te gebruiken
 #include <ArduinoJson.h>
 
@@ -22,70 +23,70 @@
 
 // Own libraries
 // variables
-#include <0main/configuration.h>
-#include <0main/globalvariables.h>
-#include <0main/config_colors.h>
-#include <0main/config_commands.h>
+#include "0main/configuration.h"
+#include "0main/globalvariables.h"
+#include "0main/config_colors.h"
+#include "0main/config_commands.h"
 
 // Own subroutines
 //   main
-#include <0main/setupSerial.h>
-//#include <0main/print.h>
+#include "0main/setupSerial.h"
 #include <PrintAnyFormat.h>
-#include <0main/save_eprom.h>
-#include <0main/setupLed.h>
-#include <0main/setupLedstrip.h>
+#include "C:\NonSyncing\Github repository\ESPtools\src\ESPtools.h"
+//#include <0main/save_eprom.h>
+#include "0main/setupLed.h"
+#include "0main/setupLedstrip.h"
 
 // commands
-#include <commands/decodeCommand.h>
-#include <commands/readcommand.h>
+#include "commands/decodeCommand.h"
+#include "commands/readcommand.h"
 
 // Spiffs() integrated file system)
-#include <spiffs/humanReadableSize.h>
-#include <spiffs/listFiles.h>
-#include <spiffs/listStorage.h>
-#include <spiffs/setupSpiffs.h>
-#include <spiffs/writeFile.h>
-#include <spiffs/readFile.h>
-#include <spiffs/fileDefault2Kring.h>
-#include <spiffs/fileLoad2Kring.h>
-#include <spiffs/fileSaveKring.h>
+#include "spiffs/humanReadableSize.h"
+#include "spiffs/listFiles.h"
+#include "spiffs/listStorage.h"
+#include "spiffs/setupSpiffs.h"
+#include "spiffs/writeFile.h"
+#include "spiffs/readFile.h"
+#include "spiffs/fileDefault2Kring.h"
+#include "spiffs/fileLoad2Kring.h"
+#include "spiffs/fileSaveKring.h"
 
 // fps
-#include <0main/show_fps.h>
+#include "0main/show_fps.h"
 
 //ledstrip
-#include <ledstrip/listKringen.h>
-#include <ledstrip/str_aan.h>
-#include <ledstrip/str_chase.h>
-#include <ledstrip/str_fire.h>
-#include <ledstrip/str_glow.h>
-#include <ledstrip/str_party.h>
-#include <ledstrip/str_uit.h>
-#include <ledstrip/showstrip.h>
-#include <ledstrip/loadAnimatie.h>
+#include "ledstrip/listKringen.h"
+#include "ledstrip/str_aan.h"
+#include "ledstrip/str_chase.h"
+#include "ledstrip/str_fire.h"
+#include "ledstrip/str_glow.h"
+#include "ledstrip/str_party.h"
+#include "ledstrip/str_uit.h"
+#include "ledstrip/showstrip.h"
+#include "ledstrip/loadAnimatie.h"
 
 //sigleled
-#include <singleled/scanI2C.h>
-#include <singleled/led_aan.h>
-#include <singleled/led_fire.h>
-#include <singleled/led_glow.h>
-#include <singleled/led_uit.h>
-#include <singleled/showled.h>
+#include "singleled/scanI2C.h"
+#include "singleled/led_aan.h"
+#include "singleled/led_fire.h"
+#include "singleled/led_glow.h"
+#include "singleled/led_uit.h"
+#include "singleled/showled.h"
 
 //wifi
-#include <wifi/on_file.h>            // downloaden of verwijderen
-#include <wifi/on_fileUpload.h>
-#include <wifi/onOTA.h>
-#include <wifi/on_getStrip.h>
-#include <wifi/on_getSetStrip.h>
-#include <wifi/on_getfileLoad2Kring.h>
-#include <wifi/on_getfileDefault2Kring.h>
-#include <wifi/on_getfileSaveKring.h>
-#include <wifi/on_getlistKringen.h>
-#include <wifi/on_wifisave.h>
-#include <wifi/setup_AsyncWebserver.h> 
-#include <wifi/setup_ConnectKnownWifi.h>
-#include <wifi/setup_CreateAccessPoint.h>
-#include <wifi/startWifi.h>
-#include <wifi/scanWifiNetworks.h>
+#include "wifi/on_file.h"            // downloaden of verwijderen
+#include "wifi/on_fileUpload.h"
+#include "wifi/onOTA.h"
+#include "wifi/on_getStrip.h"
+#include "wifi/on_getSetStrip.h"
+#include "wifi/on_getfileLoad2Kring.h"
+#include "wifi/on_getfileDefault2Kring.h"
+#include "wifi/on_getfileSaveKring.h"
+#include "wifi/on_getlistKringen.h"
+#include "wifi/on_wifisave.h"
+#include "wifi/setup_AsyncWebserver.h" 
+#include "wifi/setup_ConnectKnownWifi.h"
+#include "wifi/setup_CreateAccessPoint.h"
+#include "wifi/startWifi.h"
+#include "wifi/scanWifiNetworks.h"
