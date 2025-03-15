11980312a8315a21d829f32bb639358b10aba93a
BartelKarlijn <legoreus@gmail.com>
2025-03-15 17:05:48 +0100

    betere output

--- src/commands/CL08ledSetTimeOnOff.h
+++ src/commands/CL08ledSetTimeOnOff.h
@@ -4,7 +4,7 @@ void CL08ledSetTimeOnOff(){
     int tOn = commandParts.arg2.toInt();  //aparently toInt also converts to long
     int tOff = commandParts.arg3.toInt();
     if (commandParts.isValidCommand){ 
-        Println("L02 Set led " + commandParts.arg1 + " timeOn= " + commandParts.arg2 + ", timeOff= " + commandParts.arg3);
+        Println("L08 Set led " + commandParts.arg1 + " timeOn= " + commandParts.arg2 + ", timeOff= " + commandParts.arg3);
         if (lednr >= 0 && lednr < LED_NR_ITEMS) {
             if ((tOn >= 0) && (tOff >= 0)) {
                 ledsingle[lednr].timeon = tOn;
