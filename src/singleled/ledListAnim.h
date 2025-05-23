5b159d58075209f31b9a3e7469a03992916787fa
BartelKarlijn <legoreus@gmail.com>
2025-05-23 17:39:38 +0200

    anim zet ook struct

--- src/singleled/ledListAnim.h
+++ src/singleled/ledListAnim.h
@@ -5,14 +5,11 @@ String ledListAnim() {
 
   for (size_t i = 0; i < LED_NR_ANIM; i++)
   {
-    if(!(ledsettings[ledRequest.lednr].anim[i].animdesc == NULL)) {
       msgAnswer += "<option value='";
       msgAnswer += i;
       msgAnswer += "'>";
       msgAnswer += ledsettings[ledRequest.lednr].anim[i].animdesc;
       msgAnswer += "</option>";
-    }
-    
   }
   
   Println("settingLedListAnim: " + msgAnswer);
--- src/singleled/ledValueSet.h
+++ src/singleled/ledValueSet.h
@@ -1,5 +1,6 @@
 String ledValueSet(){
     int lednr = ledRequest.lednr;
+    int animnr = ledRequest.animnr;
     int veldnr = ledRequest.veld;
     String waardeString = ledRequest.waarde;
     String answertoSend;
@@ -11,34 +12,42 @@ String ledValueSet(){
       {
       case 50:  //we beginnen bij 50, zodat 0 een fout geeft (strip is 1-50)
         ledsingle[lednr].desc     = waardeString;
+        ledsettings[lednr].anim[animnr].animdesc = waardeString;
         answertoSend = waardeString;
         break;
       case 51:
         ledsingle[lednr].bri = waardeString.toInt();
+        ledsettings[lednr].anim[animnr].bri = waardeString.toInt();
         answertoSend = waardeString;
         break;
       case 52:
         ledsingle[lednr].effect = waardeString.toInt();
+        ledsettings[lednr].anim[animnr].effect = waardeString.toInt();
         answertoSend = waardeString;
         break;
       case 53:
         ledsingle[lednr].timeon = waardeString.toInt();
+        ledsettings[lednr].anim[animnr].timeon = waardeString.toInt();
         answertoSend = waardeString;
         break;
       case 54:
         ledsingle[lednr].timeoff = waardeString.toInt();
+        ledsettings[lednr].anim[animnr].timeoff = waardeString.toInt();
         answertoSend = waardeString;
         break;
       case 55:
         ledsingle[lednr].timeeffect = waardeString.toInt();
+        ledsettings[lednr].anim[animnr].timeeffect = waardeString.toInt();
         answertoSend = waardeString;
         break;
       case 56:
         ledsingle[lednr].image = waardeString;
+        ledsettings[lednr].ledimage = waardeString;
         answertoSend = waardeString;
         break;
       case 57:
         ledsingle[lednr].animatie = waardeString;
+        ledsettings[lednr].anim[animnr].animdesc = waardeString;
         answertoSend = waardeString;
         break;
       default:
