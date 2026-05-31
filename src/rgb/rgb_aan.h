d577e2a5667dbfb8c8b0308f5be8b002cbf462cd
BartelKarlijn <legoreus@gmail.com>
2026-05-31 18:22:14 +0200

    aan met rampup en down

--- src/rgb/rgb_aan.h
+++ src/rgb/rgb_aan.h
@@ -7,33 +7,72 @@ void rgb_aan(size_t rgbnr) {
 
   bool fl_on;
   unsigned long timeBezig;
+  uint16_t bri1, bri2;
 
   timeBezig = currentMillis - timer_rgb_aan[rgbnr];
+  #define RGB_AAN_FASE_NEWCYCLE   0
+  #define RGB_AAN_FASE_RAMPUP     1
+  #define RGB_AAN_FASE_FULLON     2
+  #define RGB_AAN_FASE_RAMPDOWN   3
+  #define RGB_AAN_FASE_OFF        4
+  uint8_t fase = 0;
 
+  // bepaal fase; dit moet van achter naar voor gebeuren
   if( timeBezig > (rgbactive[rgbnr].timeon + rgbactive[rgbnr].timeoff) ) {   // lang genoeg uit geweest.  Nu aanzetten
-    fl_on = true;
-    timer_rgb_aan[rgbnr] = currentMillis; //reset timer
+    fase = RGB_AAN_FASE_NEWCYCLE;
   }
   else  if (timeBezig > rgbactive[rgbnr].timeon) { // lang genoeg aan geweest
-    fl_on = false;
+    fase = RGB_AAN_FASE_OFF;
   }
-  else {     //laat nog maar tijdje aan.
-    fl_on = true;
+  else  if ((timeBezig > rgbactive[rgbnr].timeon - rgbactive[rgbnr].timeeffect)
+         && (timeBezig > rgbactive[rgbnr].timeeffect ) ) { // checken dat we geen t.effect wel groot genoeg is.
+    fase = RGB_AAN_FASE_RAMPDOWN;
+  }
+  else  if ((timeBezig > rgbactive[rgbnr].timeeffect ) ) { 
+    fase = RGB_AAN_FASE_FULLON;
+  }
+  else {     
+    fase = RGB_AAN_FASE_RAMPUP;
   }
 
-  for (size_t i = rgbactive[rgbnr].startrgb; i <= rgbactive[rgbnr].stoprgb; i++)
+  // Nu uitvogelen welke kleur enz te gebruiken
+  switch (fase)
   {
-    // aan of uit zetten?
-    if (fl_on) {
-      if( ( (i - rgbactive[rgbnr].startrgb + rgbactive[rgbnr].seed + 30*rgbactive[rgbnr].every) % rgbactive[rgbnr].every ) == 0 ) {  // check every.  Aangezien negatieve waarden kunnen voorkomen, tellen we er 30 x every bij op om zeker positieve waarden te hebben
-        rgbstrip[i] = CHSV(rgbactive[rgbnr].hue1, rgbactive[rgbnr].sat1, rgbactive[rgbnr].bri1 );
-      }
-      else {
-        rgbstrip[i] = CHSV(rgbactive[rgbnr].hue2, rgbactive[rgbnr].sat2, rgbactive[rgbnr].bri2 );
-      }
+  case RGB_AAN_FASE_NEWCYCLE:
+    timer_rgb_aan[rgbnr] = currentMillis; //reset timer
+    bri1 = 0;
+    bri2 = 0;
+    break;
+  case RGB_AAN_FASE_FULLON:
+    bri1 = rgbactive[rgbnr].bri1;
+    bri2 = rgbactive[rgbnr].bri2;
+    break;
+  case RGB_AAN_FASE_OFF:
+    bri1 = 0;
+    bri2 = 0;
+    break;
+  case RGB_AAN_FASE_RAMPUP:
+    bri1 = map(timeBezig, 0, rgbactive[rgbnr].timeeffect, 0, rgbactive[rgbnr].bri1);
+    bri2 = map(timeBezig, 0, rgbactive[rgbnr].timeeffect, 0, rgbactive[rgbnr].bri2);
+    break;
+  case RGB_AAN_FASE_RAMPDOWN:
+    bri1 = map(timeBezig, rgbactive[rgbnr].timeon - rgbactive[rgbnr].timeeffect, rgbactive[rgbnr].timeon, rgbactive[rgbnr].bri1, 0);
+    bri2 = map(timeBezig, rgbactive[rgbnr].timeon - rgbactive[rgbnr].timeeffect, rgbactive[rgbnr].timeon, rgbactive[rgbnr].bri2, 0);
+    break;
+  default:
+    bri1 = 0;
+    bri2 = 0;
+    break;
+  }
+  
+  // Tenslotte alle ledjes de juiste kleur geven  
+  for (size_t i = rgbactive[rgbnr].startrgb; i <= rgbactive[rgbnr].stoprgb; i++) {
+    if( ( (i - rgbactive[rgbnr].startrgb + rgbactive[rgbnr].seed + 30*rgbactive[rgbnr].every) % rgbactive[rgbnr].every ) == 0 ) {  // check every.  Aangezien negatieve waarden kunnen voorkomen, tellen we er 30 x every bij op om zeker positieve waarden te hebben
+      rgbstrip[i] = CHSV(rgbactive[rgbnr].hue1, rgbactive[rgbnr].sat1, bri1 );
     }
     else {
-      rgbstrip[i] = CRGB::Black;
+      rgbstrip[i] = CHSV(rgbactive[rgbnr].hue2, rgbactive[rgbnr].sat2, bri2 );
     }
   }
+  
 }
