0717dc652747d6c1ca97e7e14d8515b206d186af
BartelKarlijn <legoreus@gmail.com>
2026-06-07 15:09:23 +0200

    cfg files

--- src/led/led_delay.h
+++ src/led/led_delay.h
@@ -5,11 +5,11 @@ void led_delay(size_t lednr) {
 
   timeBezig = currentMillis - timer_led_aan[lednr];
 
-  if( timeBezig > (ledactive[lednr].timeon + ledactive[lednr].timeoff) ) {   // lang genoeg uit geweest.  Nog niet aanzetten, eerst delay uitzitten
+  if( timeBezig > (ledactive[lednr].timeeffect + ledactive[lednr].timeon + ledactive[lednr].timeoff) ) {   // lang genoeg uit geweest.  Nog niet aanzetten, eerst delay uitzitten
     ledPCA9685.setPWM(lednr, 0, 0);
     timer_led_aan[lednr] = currentMillis; //reset timer
   }
-  else  if (timeBezig > ledactive[lednr].timeon) { // lang genoeg aan geweest
+  else  if (timeBezig > (ledactive[lednr].timeeffect + ledactive[lednr].timeon)) { // lang genoeg aan geweest
     ledPCA9685.setPWM(lednr, 0, 0);
   }
   else  if (timeBezig > ledactive[lednr].timeeffect) { //delay uitgezeten
